7effd33ee16ceeb8c080159ea4876d96103f829a
pravriti <jaipuriy@rhrk.uni-kl.de>
2023-06-02 18:56:12 +0200

    Modified for saving model and readability

--- wordPattern.py
+++ wordPattern.py
@@ -7,19 +7,20 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+from pathlib import Path
 from gensim.models import Word2Vec, KeyedVectors
 
 verbose = 0
-maxlength = 10
+maxlength = 20
 
 # Define hyperparameters
 input_size = 300
 hidden_size = 64
 output_size = 1
-num_layers = 1
+num_layers = 2
 # Define hyperparameters
 learning_rate = 0.001
-num_epochs = 15
+num_epochs = 100
 batch_size = 32
 
 # Define the loss function
@@ -33,8 +34,8 @@ def load_model():
     word2vecPath = "./models/GoogleNews-vectors-negative300.bin"
     warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
     if not os.path.exists(word2vecPath):
-        raise Exception("word2vec model doesn't exist at the path:" + str(word2vecPath))
-    debug(0,"Model at " + str(word2vecPath))
+        raise Exception("Error: word2vec model doesn't exist at the path:" + str(word2vecPath))
+    debug(0,f"Info: word2vec Model at {word2vecPath}")
 
     if (verbose==2):
         process = psutil.Process(os.getpid())
@@ -46,7 +47,7 @@ def load_model():
         # mem= psutil.virtual_memory()
         # ttl = mem.total  # Toal memory available
 
-    w2v_model = KeyedVectors.load_word2vec_format(word2vecPath, binary=True, limit=20000)  # load the model
+    w2v_model = KeyedVectors.load_word2vec_format(word2vecPath, binary=True)#, limit=20000)  # load the model
     # w2v_model = gensim.models.KeyedVectors.load_word2vec_format('./models/GoogleNews-vectors-negative300.bin', binary=True)
     if (verbose==2):
         print("%0.2f seconds taken to load" % float(time.time() - start_time))  # Calculate the total time elapsed since starting the timer
@@ -64,7 +65,7 @@ def load_model():
             float((post / pre) * 100)))  # Percentage increase in memory after loading the model
         print('-' * 10)
 
-        print("Number of words in vocabs: ", len(w2v_model))  # Number of words in the vocabulary
+    debug(0,f"Number of words in vocabs: {len(w2v_model)}")  # Number of words in the vocabulary
     return w2v_model
 def test_model(w2v_model):
     # word = w2v_model[100]
@@ -102,13 +103,13 @@ def generateTypeExamples(num_examples, pos):
         k = random.randint(1, maxlength)
         if pos:
             word = random.choices(w2v_model.index_to_key)
-            if k%2!=0: #To add '# at the end of positive examples if k is -ve
-                k +=1
-            wordL.append([word[0] if i % 2 ==0  else '#' for i in range(k)])
+            if k%2 != 0: #To add '# at the end of positive examples if k is -ve
+                k += 1
+            wordL.append([word[0] if i%2 == 0  else '#' for i in range(k)])
         else:
             x = []
             for i in range(k):
-                if i%2==0:
+                if i%2 == 0:
                     word = random.choices(w2v_model.index_to_key)
                     x.append(word[0])
                 else:
@@ -127,7 +128,7 @@ def generateExamples(num_examples):
     if negL_count != 0:
         negL = negL + generateTypeExamples(negL_count, pos=False)
     debug(2, "positive examples:" + str(posL))
-    debug(2,"negative examples:" + str(negL))
+    debug(2, "negative examples:" + str(negL))
     return posL, negL
 
 def encode_sequence(sequence, w2v_model):
@@ -140,6 +141,14 @@ def encode_sequence(sequence, w2v_model):
     debug(2, "target:" + str(target_seq))
     return target_seq
 
+def special_word():
+    #change the code for appending any special word for testing RNN
+    k = random.randint(1, maxlength)
+    if k%2 != 0:
+        k += 1
+    special_sentence = ["Jaipuriyar" if i%2 == 0 else "#" for i in range(k)]
+    return special_sentence
+
 def create_datasets(num_examples, w2v_model, train=False):
     X = []
     y = []
@@ -151,13 +160,15 @@ def create_datasets(num_examples, w2v_model, train=False):
         y.append(1 if sequence in posL else 0)
     debug(2, "X =" + str(X))
     debug(2, "y=" + str(y))
-    print("Total number of samples generated:", num_examples)
+    print(f"Info: Total number of samples generated: {num_examples}")
     if train:
-        print("Number of positive examples in training:", y.count(1))
-        print("Number of negative examples in training:", y.count(0))
+        print(f"Info: Number of positive examples in training: {y.count(1)}")
+        print(f"Info: Number of negative examples in training: {y.count(0)}")
     else:
-        print("Number of positive examples in test:", y.count(1))
-        print("Number of negative examples in test:", y.count(0))
+        #X.append(encode_sequence(special_word(), w2v_model))
+        #y.append(1)
+        print(f"Info: Number of positive examples in test: {y.count(1)}")
+        print(f"Info: Number of negative examples in test: {y.count(0)}")
     return X, torch.tensor(y, dtype=torch.float)
 
 
@@ -172,8 +183,8 @@ def checkAccuracy(predicted, actual):
                 else:
                     neg += 1
 
-    print("Number of +ve samples identified correctly:", pos)
-    print("Number of -ve samples identified correctly:", neg)
+    print(f"Info: Number of +ve samples identified correctly: {pos}")
+    print(f"Info: Number of -ve samples identified correctly: {neg}")
 
 
 def createRNNModel():
@@ -198,10 +209,26 @@ def createRNNModel():
     model = RNN(input_size, hidden_size, output_size, num_layers).to(device)
     return model
 
+def load_RNN_model(model):
+    model_path = "./models/modelRNN.pt"
+    needTraining = False
+    if not os.path.exists(model_path):
+        # raise Exception (f"No model exists at {model_path}, "
+        #                  f"Please try creating a new RNN model or check the path or model name again")
+        printf(f"Warning: Model doesn't exist at path {model_path}, So training is needded for a new RNN model")
+        needTraining = True
+    else:
+        print(f"Info: RNN Model loaded Successfully!")
+        model.load_state_dict(torch.load(model_path))
+        print(f'Info: Model {model}')
+    return model, needTraining
+
 def train_RNN(model, X_train, y_train, device):
     # Define the optimizer
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    patience, patience_counter = 50, 0
+    best_avg_loss = 1
     # Training loop
     for epoch in range(num_epochs):
         total_loss = 0
@@ -223,7 +250,19 @@ def train_RNN(model, X_train, y_train, device):
             total_loss += loss.item()
 
         avg_loss = total_loss / (len(X_train) / batch_size)
-        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss:.4f}")
+
+        if avg_loss < best_avg_loss:
+            best_avg_loss = avg_loss
+            torch.save(model.state_dict(), Path("models/", "modelRNN.pt"))
+            print(f'Info: Epoch {epoch} best Model saved with the loss {best_avg_loss}')
+
+        else:
+            patience_counter +=1
+            if patience_counter >= patience:
+                print(f'Early stopping on epoch {epoch}')
+                break
+
+        print(f'Info: Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss:.4f}')
 
 def test_RNN(model, X_test, y_test, device):
 # Evaluation
@@ -232,7 +271,8 @@ def test_RNN(model, X_test, y_test, device):
         # X_test_padded = pad_sequences(X_test)
         # X_test_padded = torch.stack(X_test_padded).to(device)
         X_test = torch.stack(X_test).to(device)
-        y_test = torch.tensor(y_test, dtype=torch.float).unsqueeze(1).to(device)
+        # y_test = torch.tensor(y_test, dtype=torch.float).unsqueeze(1).to(device)
+        y_test = torch.as_tensor(y_test, dtype=torch.float).clone().detach().unsqueeze(1).to(device)
         outputs = model(X_test)
         loss = criterion(outputs, y_test)
         predicted_labels = torch.round(outputs)
@@ -243,21 +283,29 @@ def test_RNN(model, X_test, y_test, device):
         # print("sum:", (predicted_labels==y_test).sum())
         # print("item:", (predicted_labels == y_test).sum().item())
         checkAccuracy(predicted_labels.numpy(),y_test.numpy())
-        print(f"Test Loss: {loss.item():.4f}, Test Accuracy: {accuracy:.4f}")
+        print(f"Info: Test Loss: {loss.item():.4f}, Test Accuracy: {accuracy:.4f}")
 
 
 if __name__ == "__main__":
     w2v_model = load_model()
     test_model(w2v_model)
-    X_train, y_train = create_datasets(10000, w2v_model=w2v_model, train=True)
-    X_test, y_test = create_datasets(1000, w2v_model=w2v_model, train=False)
-    print("Length of X(input) for training:", len(X_train))
-    print("Size of y(label) tensor for training:", y_train.size())
-    print("Length of X(input) for testing:", len(X_test))
-    print("Size of y(label) tensor for testing:", y_test.size())
-
     # Set the device for training
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     RNN_model = createRNNModel()
-    train_RNN(RNN_model, X_train, y_train, device)
+    # Load the model saved already in the models folder
+    RNN_model, needTraining = load_RNN_model(RNN_model)
+
+    if needTraining:
+        X_train, y_train = create_datasets(10000, w2v_model=w2v_model, train=True)
+        print(f"Info: Length of X(input) for training: {len(X_train)}")
+        print(f"Info: Size of y(label) tensor for training: {y_train.size()}")
+        train_RNN(RNN_model, X_train, y_train, device)
+    else:
+        print(f"Info: Training is skipped!")
+
+    X_test, y_test = create_datasets(1000, w2v_model=w2v_model, train=False)
+    print(f"Info: Length of X(input) for testing: {len(X_test)}")
+    print(f"Info: Size of y(label) tensor for testing: {y_test.size()}")
+
+
     test_RNN(RNN_model, X_test, y_test, device)
