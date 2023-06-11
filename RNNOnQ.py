7a97d6fcc687f3f2ca86c4c9e1bddd8aaca2a276
pravriti <jaipuriy@rhrk.uni-kl.de>
2023-06-11 22:41:40 +0200

    Adding RNN on Q and fixing bugs on wordPatter

--- RNNOnQ.py
+++ RNNOnQ.py
@@ -14,17 +14,18 @@ from gensim.models import Word2Vec, KeyedVectors
 
 verbose = 0
 maxlength = 20
+UpperBound = 9999999999
 
 # Define hyperparameters
-input_size = 300
+input_size = 1
 hidden_size = 64
 output_size = 1
 num_layers = 2
 # Define hyperparameters
-learning_rate = 0.001
-num_epochs = 100
+learning_rate = 0.0001
+num_epochs = 500
 batch_size = 32
-modelName = "modelRNN.pt"
+modelName = "modelRNNOnQ.pt"
 
 # Define the loss function
 criterion = nn.BCELoss()
@@ -33,190 +34,133 @@ def debug(verbose_level, str):
     if verbose >= verbose_level:
         print(str)
 
-# W2V Model:
-def load_model():
-    word2vecPath = "./models/GoogleNews-vectors-negative300.bin"
-    warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
-    if not os.path.exists(word2vecPath):
-        raise Exception("Error: word2vec model doesn't exist at the path:" + str(word2vecPath))
-    debug(0,f"Info: word2vec Model at {word2vecPath}")
-
-    if (verbose==2):
-        process = psutil.Process(os.getpid())
-        pre = process.memory_info().rss
-        print("Memory used in GB before Loading the Model: %0.2f" % float(pre / (10 ** 9)))  # Check memory usage before loading the model
-        print('-' * 10)
-
-        start_time = time.time()  # Start the timer
-        # mem= psutil.virtual_memory()
-        # ttl = mem.total  # Toal memory available
-
-    w2v_model = KeyedVectors.load_word2vec_format(word2vecPath, binary=True)#, limit=20000)  # load the model
-    if (verbose==2):
-        print("%0.2f seconds taken to load" % float(time.time() - start_time))  # Calculate the total time elapsed since starting the timer
-        print('-' * 10)
-
-        print('Finished loading Word2Vec')
-        print('-' * 10)
-
-        post = process.memory_info().rss
-        print("Memory used in GB after Loading the Model: {:.2f}".format(
-            float(post / (10 ** 9))))  # Calculate the memory used after loading the model
-        print('-' * 10)
-
-        print("Percentage increase in memory usage: {:.2f}% ".format(
-            float((post / pre) * 100)))  # Percentage increase in memory after loading the model
-        print('-' * 10)
-
-    debug(0,f"Info: Number of words in vocabs: {len(w2v_model)}")  # Number of words in the vocabulary
-    return w2v_model
-def test_model(w2v_model):
-    # word = w2v_model[100]
-    word = random.choices(w2v_model.index_to_key)
-    # array_for_hash = w2v_model.get_vector("#")
-    # print(array_for_hash)
-    if not(word):
-        raise Exception("w2v Model not loaded properly!")
-    debug (0, "Info: w2v Model loading tested Successfully!")
-
-def update_w2vModelVocab(newSentence, model):
-    pass
-#     newModelPath = "/models/w2v_model_updatedVersion.bin"
-#     new_words = newSentence
-#     # # class SubscriptableWord2Vec(Word2Vec):
-#     # #     def __getitem__(self, word):
-#     # #         return self.wv[word]
-#     #
-#     # extended_model = Word2Vec(vector_size=w2v_model.vector_size, min_count=1)
-#     # extended_model.build_vocab_from_freq({word: 1 for word in new_words})
-#     # extended_model.wv.vectors = np.concatenate([w2v_model.vectors, extended_model.wv.vectors], axis=0)
-#     # # extended_model.wv.vectors = extended_vectors
-#     # # extended_model.save(newModelPath)
-#     # # save_word2vec_format(extended_model, newModelPath, binary=True)
-#     # extended_model.wv.save_word2vec_format(newModelPath, binary=True)
-#     # # extended_model = KeyedVectors.load(newModelPath)
-#
-#     # Determine the vector size of the model
-#     vector_size = model.vector_size
-#
-#     # Create random vectors for the new words
-#     new_vectors = np.random.random((len(new_words), vector_size))
-#
-#     # Update the model with new words and vectors
-#     model.add_vectors(new_words, new_vectors)
-#
-#     # Save the updated model
-#     model.save(newModelPath)
-#     # model.save_word2vec_format(newModelPath, binary=True)
-#     return model
-
-def generateNewWord():
-    # return a list of new words which is not present in the default vocab of w2v model:
-    # word = random.randint(1, 999999999999999999)
-    # return ["915977186963600798"]
-    # return ["Jaipuriyar"]
-    return []
 def special_word():
     # change the code for appending any special word for testing RNN
     word = generateNewWord()
     k = random.randint(1, maxlength)
     print(f'special word chosen is : {word} and sentence length is: {k}')
-    if k%2 != 0:
+    if k % 2 != 0:
         k += 1
-    special_sentence = [word if i%2 == 0 else "#" for i in range(k)]
+    special_sentence = [word if i % 2 == 0 else "#" for i in range(k)]
     return special_sentence
 
-def genSpecialNegExample(num_examples):
+def genSpecialNegExample(num_examples, lang):
     # TODO: create examples without '#' as well : Already added due to length selection
     wordL = []
     while num_examples > 0:
-        k = random.randint(1, maxlength-2)
-        word = random.choices(w2v_model.index_to_key)
-        x = [word[0] if i % 2 == 0 else '#' for i in range(k)]
-        if x[-1] != '#':
+        if lang == 1 : #L = (a#)^n
+            k = random.randint(1, maxlength-2)
+            word = random.randint(1,UpperBound)
+            x = [word if i % 2 == 0 else '#' for i in range(k)]
+            if x[-1] != '#':
+                x.append('#')
+            word = random.randint(1,UpperBound)
+            while word == x[0]:
+                word = random.randint(1,UpperBound)
+            x.append(word)
             x.append('#')
-        word = random.choices(w2v_model.index_to_key)
-        while word == x[0]:
-            word = random.choices(w2v_model.index_to_key)
-        x.append(word[0])
-        x.append('#')
-        k = random.randint(0, maxlength-len(x))
-        y = [x[0] if i%2 ==0 else '#' for i in range(k)]
-        x = x + y
-        wordL.append(x)
+            k = random.randint(0, maxlength-len(x))
+            y = [x[0] if i%2 == 0 else '#' for i in range(k)]
+            x = x + y
+            wordL.append(x)
+
+        elif lang == 2 : # L = first and last letter equal
+            break
+
         num_examples -= 1
     # print("special Negatives:", wordL)
     return wordL
-def generateTypeExamples(num_examples, pos):
+def generateTypeExamples(num_examples, lang, pos):
     wordL = []
     while num_examples > 0:
         k = random.randint(1, maxlength)
-        if pos:
-            word = random.choices(w2v_model.index_to_key)
-            if k%2 != 0: #To add '# at the end of positive examples if k is -ve
-                k += 1
-            wordL.append([word[0] if i%2 == 0  else '#' for i in range(k)])
-        else:
-            x = []
-            for i in range(k):
-                if i%2 == 0:
-                    word = random.choices(w2v_model.index_to_key)
-                    x.append(word[0])
+
+        # L = (a#)^n where a \in Q+ and # is encoded as 0.
+        if lang == 1:
+            if pos:
+                word = random.randint(1,UpperBound)
+                if k % 2 != 0: #To add '# at the end of positive examples if k is odd
+                    k += 1
+                wordL.append([word if i%2 == 0  else '#' for i in range(k)])
+            else:
+                x = []
+                for i in range(k):
+                    if i % 2 == 0:
+                        word = random.randint(1,UpperBound)
+                        x.append(word)
+                    else:
+                        x.append("#")
+                wordL.append(x)
+
+        # L = axa, axya # first and last letter is same
+        elif lang == 2:
+            k = 2 * k  # will drop the delimiter symbol '#'  while encoding
+            if k > 2:
+                x = [random.randint(1, UpperBound) if i % 2 == 0 else '#' for i in range(k - 2)]
+                # print(f"length of x:{len(x)} and chosen k is {k/2}")
+                if pos:
+                    x.append(x[0])
                 else:
-                    x.append("#")
+                    p = random.randint(1, UpperBound)
+                    while p == x[0]:
+                        p = random.randint(1, UpperBound)
+                    x.append(p)
+            else:
+                x = [random.randint(1, UpperBound)]
+            x.append('#')
             wordL.append(x)
+
         num_examples -= 1
+
     return wordL
 
-def generateExamples(num_examples):
+def generateExamples(num_examples, lang):
     posL_count = int(num_examples / 2)
     negL_count = num_examples - posL_count
-    negL = genSpecialNegExample(int(negL_count / 2))
-    negL_count = negL_count - int(negL_count / 2)
+    negL = genSpecialNegExample(int(negL_count / 2), lang)
+    negL_count = negL_count - len(negL)
     if posL_count != 0:
-        posL = generateTypeExamples(posL_count, pos=True)
+        posL = generateTypeExamples(posL_count, lang=lang, pos=True )
     if negL_count != 0:
-        negL = negL + generateTypeExamples(negL_count, pos=False)
+        negL = negL + generateTypeExamples(negL_count, lang=lang, pos=False)
     debug(2, "positive examples:" + str(posL))
     debug(2, "negative examples:" + str(negL))
     return posL, negL
 
-def encode_sequence(sequence, w2v_model):
-    debug(1, "sequence:" + str(sequence))
-    x = torch.tensor(np.array([w2v_model[word] for word in sequence]))
-    debug(1, "size of x:" + str(x.size()))
-    target_seq = torch.zeros(maxlength,300)
+def encode_sequence(sequence, lang):
+    debug(2, "sequence:" + str(sequence))
+    # x = torch.tensor(np.array([w2v_model[word] for word in sequence]))
+    if lang == 1:
+        x = torch.tensor(np.array([[word] if word != '#' else [0]  for word in sequence]))
+    elif lang == 2:
+        x = torch.tensor(np.array([[word] for word in sequence if word != '#']))
+    debug(1, f"size of x: {x.size()}")
+    target_seq = torch.zeros(maxlength, 1)
     target_seq[0:x.size(0),:] = x
-    debug(1, "size of target:" + str(target_seq.size()))
-    debug(2, "target:" + str(target_seq))
+    debug(1, f"size of target: {target_seq.size()}")
+    debug(2, f"target: {target_seq}")
     return target_seq
 
-def create_datasets(num_examples, w2v_model, train=False):
+def create_datasets(num_examples, lang, train=False):
     X = []
     y = []
-    # update the w2v_model with new words, if we want to test RNN model on words outside the default vocab of w2v model.
-    newWordList = generateNewWord()
-    if newWordList != []:
-        w2v_model = update_w2vModelVocab(newWordList, w2v_model)
-
-    posL, negL = generateExamples(num_examples)
+    print(f"Info: Generating train={train} examples...")
+    posL, negL = generateExamples(num_examples, lang)
     result = posL + negL
     random.shuffle(result)
     for sequence in result:
-        X.append(encode_sequence(sequence, w2v_model))
+        X.append(encode_sequence(sequence, lang))
         y.append(1 if sequence in posL else 0)
-    debug(2, "X =" + str(X))
-    debug(2, "y=" + str(y))
+    debug(0, "X =" + str(X))
+    debug(0, "y=" + str(y))
     print(f"Info: Total number of samples generated: {num_examples}")
     if train:
         print(f"Info: Number of positive examples in training: {y.count(1)}")
         print(f"Info: Number of negative examples in training: {y.count(0)}")
     else:
-        if newWordList != []:
-            X.append(encode_sequence(special_word(), w2v_model))
-            y.append(1)
         print(f"Info: Number of positive examples in test: {y.count(1)}")
         print(f"Info: Number of negative examples in test: {y.count(0)}")
+    print(f"Info: Finished Generating train={train} examples.")
     return X, torch.tensor(y, dtype=torch.float)
 
 
@@ -263,7 +207,7 @@ def load_RNN_model(model):
     if not os.path.exists(model_path):
         # raise Exception (f"No model exists at {model_path}, "
         #                  f"Please try creating a new RNN model or check the path or model name again")
-        print(f"Warning: Model doesn't exist at path {model_path}, So training is needed for a new RNN model")
+        print(f"Warning: Model doesn't exist at path {model_path}, So training is needded for a new RNN model")
         needTraining = True
     else:
         print(f"Info: RNN Model loaded Successfully!")
@@ -299,7 +243,7 @@ def train_RNN(model, X_train, y_train, device):
 
         avg_loss = total_loss / (len(X_train) / batch_size)
 
-        if avg_loss < best_avg_loss:
+        if avg_loss <= 0.99*best_avg_loss:
             best_avg_loss = avg_loss
             torch.save(model.state_dict(), Path("models/", modelName))
             print(f'Info: Epoch {epoch} best Model saved with the loss {best_avg_loss}')
@@ -336,23 +280,25 @@ def test_RNN(model, X_test, y_test, device):
 
 
 if __name__ == "__main__":
-    w2v_model = load_model()
-    test_model(w2v_model)
     # Set the device for training
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     RNN_model = createRNNModel()
     # Load the model saved already in the models folder
     RNN_model, needTraining = load_RNN_model(RNN_model)
 
+    #select langugage:
+    lang = 2
+    needTraining = True
+
     if needTraining:
-        X_train, y_train = create_datasets(10000, w2v_model=w2v_model, train=True)
+        X_train, y_train = create_datasets(100000, lang, train=True)
         print(f"Info: Length of X(input) for training: {len(X_train)}")
         print(f"Info: Size of y(label) tensor for training: {y_train.size()}")
         train_RNN(RNN_model, X_train, y_train, device)
     else:
         print(f"Info: Training is skipped!")
 
-    X_test, y_test = create_datasets(1000, w2v_model=w2v_model, train=False)
+    X_test, y_test = create_datasets(1000, lang, train=False)
     print(f"Info: Length of X(input) for testing: {len(X_test)}")
     print(f"Info: Size of y(label) tensor for testing: {y_test.size()}")
     test_RNN(RNN_model, X_test, y_test, device)
