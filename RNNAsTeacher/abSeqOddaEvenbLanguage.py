97cd22130f3e769f02cb3fb78ec1d326e77e87d7
pravriti <jaipuriy@rhrk.uni-kl.de>
2023-06-18 14:28:29 +0200

    Added example and ground truth function for 3rd lang

---
+++ RNNAsTeacher/abSeqOddaEvenbLanguage.py
@@ -0,0 +1,135 @@
+'''
+Language accepted is: (a)^n(b)^m,
+where n is an odd number and b is a an even number where b must be greater then 0.
+'''
+import sys
+import os
+import random
+
+modelDir = "../"
+pathsToInclude = ["../../TheoryOfEquality/vLStarForRationalAutomata/", modelDir, "../RNNLanguageModels"]
+for path in pathsToInclude:
+    sys.path.append(path)
+
+from vLStar import RationalNumber, RationalNominalAutomata, learn
+
+from rnnInterface import RNNInterface
+from GTComparison import GTComparison
+from groundTruthFunctions import Lang_is_abSeq_OddaEvenb
+
+RNNModelName = "modelRNN_lang3_aOdd_must_followed_by_bEven.pt"
+RNNModelPath = os.path.join(modelDir, "models", RNNModelName)
+word2vecModelPath = os.path.join(modelDir, "models", "GoogleNews-vectors-negative300.bin")
+
+samples = [[RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(1, 1)],
+           [RationalNumber(1, 1), RationalNumber(0, 1)],
+           [RationalNumber(1, 1), RationalNumber(1, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(1, 1)],
+           [RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(1, 1)],
+           [RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(0, 1)],
+           [RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(1, 1)],
+           [RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(0, 1)],
+           [RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(1, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(1, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(1, 1)],
+           [RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(1, 1)],
+           [RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(1, 1)],
+           [RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1)],
+           [RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(1, 1)],
+           [RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(0, 1)],
+           [RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(1, 1), RationalNumber(1, 1)],
+           [RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(0, 1)],
+           [RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(0, 1), RationalNumber(1, 1)],
+           [RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(0, 1)],
+           [RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(1, 1), RationalNumber(1, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1),
+            RationalNumber(0, 1)],
+           [RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1), RationalNumber(0, 1),
+            RationalNumber(0, 1), RationalNumber(0, 1)]]
+
+rnnInterface = RNNInterface(word2vec_model_path=word2vecModelPath, rnn_model_path=RNNModelPath)
+gTComparison = GTComparison(Lang_is_abSeq_OddaEvenb)
+
+
+def membershipQuery(word: list, printing=True) -> bool:
+    # expects a list of RationalNumbers
+    if len(word) and type(word[0]) != type(RationalNumber(None, None)):
+        raise Exception("membershipQuery was called with the list: " + str(word) + "\n of type: " + str(type(word)))
+    print(f"The query is: {word}")
+    rnnReply = rnnInterface.askRNN(word)
+    Qreply = gTComparison.getGT(word, rnnReply, printing)
+    # print (Qreply)
+    if Qreply:
+        if printing:
+            print(f"membershipQuery: {word} is in the language.")
+    else:
+        if printing:
+            print(f"membershipQuery: {word} is not in the language.")
+    return Qreply
+
+
+def statisticalEquivalenceQuery(automaton: RationalNominalAutomata) -> tuple:
+    global samples
+    numberOfExamples = 100
+
+    print("Checking equivalence of the following automaton:")
+    print(automaton)
+
+    for word in samples:
+        isMember = membershipQuery(word, False)
+        hypothesisIsMember = automaton.accepts(word)
+
+        if isMember != hypothesisIsMember:
+            print("The languages are not equivalent, a counterexample is: " + str(word))
+            if isMember:
+                print("The word was rejected, but is in the language.")
+            else:
+                print("The word was accepted, but is not in the language.")
+            return (False, word)
+        else:
+            if isMember:
+                print(str(word) + " was correctly accepted")
+            else:
+                print(str(word) + " was correctly rejected")
+
+    for l in range(numberOfExamples):  # + numberOfEquivalenceQueriesAsked):
+        length = 1 + int(l / 20)
+        numerators = [random.randint(1, 5) for i in range(length)]  # random.sample(range(0, 5), length)
+        denominators = [1] * length  # random.sample(range(1, 10 + 2 * l), length)
+        word = [RationalNumber(numerators[i], denominators[i]) for i in range(length)]
+
+        isMember = membershipQuery(word, False)
+        hypothesisIsMember = automaton.accepts(word)
+
+        if isMember != hypothesisIsMember:
+            print("The languages are not equivalent, a counterexample is: " + str(word))
+            if isMember:
+                print("The word was rejected, but is in the language.")
+            else:
+                print("The word was accepted, but is not in the language.")
+            return (False, word)
+        else:
+            if isMember:
+                print(str(word) + " was correctly accepted")
+            else:
+                print(str(word) + " was correctly rejected")
+    print("The languages appear to be  equivalent after checking " + str(numberOfExamples) + " random examples")
+    return (True, None)
+
+
+def main() -> None:
+    learnedAutomaton = learn(membershipQuery, statisticalEquivalenceQuery)
+    print(learnedAutomaton)
+    gTComparison.statistics()
+
+
+if __name__ == "__main__":
+    main()
--- RNNLanguageModels/groundTruthFunctions.py
+++ RNNLanguageModels/groundTruthFunctions.py
@@ -27,4 +27,27 @@ def Lang_is_abSeq(word: list):
 
     return False
 
+def Lang_is_abSeq_OddaEvenb(word:list):
+    ''' This functions gives correct/ ground Truth for the language a^nb^m where
+    where n is an odd number and b is a an even number where b must be greater then 0 '''
+    if len(word) and type(word[0]) != type(RationalNumber(None, None)):
+        raise Exception("membershipQuery was called with the list: " + str(word) + "\n of type: " + str(type(word)))
+
+    s  = list(set(word))
+    if len(s) != 2:
+        return False
+
+    if s[0] != word[0]:
+        s[0], s[1] = s[1], s[0]
+
+    i, count_a = 0, 0
+    while i < len(word) and s[0] == word[i]:
+        count_a += 1
+        i += 1
+    if all([word[i]==s[1] for i in range(i, len(word))]) and count_a % 2 == 1 \
+            and word.count(s[1]) %2 == 0: #language acceptance criteria
+        return True
+
+    return False
+
 
