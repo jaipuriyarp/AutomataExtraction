93ab7a3e7caac0cede1945903c3ef506b8c6b684
pravriti <jaipuriy@rhrk.uni-kl.de>
2023-06-18 13:24:51 +0200

    Added aStar example + modified abSeq. Example str

--- RNNAsTeacher/abSequenceLanguage.py
+++ RNNAsTeacher/abSequenceLanguage.py
@@ -2,7 +2,7 @@
 import sys
 import os
 import random
-import torch
+# import torch
 
 modelDir = "../"
 pathsToInclude = ["../../TheoryOfEquality/vLStarForRationalAutomata/", modelDir, "../RNNLanguageModels"]
@@ -10,17 +10,14 @@ for path in pathsToInclude:
     sys.path.append(path)
 
 from vLStar import RationalNumber, RationalNominalAutomata, learn
-from languageGenerator import load_model, encode_sequence
-from rnnModel import RNNModel
-from GTComparison import GTComparison
-from groundTruthFunctions import Lang_is_abSeq
 
-RNNModelName = "modelRNN_abSeq.pt"
+from rnnInterface import RNNInterface
+from GTComparison import GTComparison
+from groundTruthFunctions import Lang_is_abSeq, Lang_is_aStar
+
+RNNModelName = "modelRNN_lang2_abSeq.pt"
 RNNModelPath =  os.path.join(modelDir, "models", RNNModelName)
 word2vecModelPath = os.path.join(modelDir, "models", "GoogleNews-vectors-negative300.bin")
-
-UpperBound = 3000000 #number of words in Google's word2vec
-debug = False
 
 samples = [[RationalNumber(0,1)],
            [RationalNumber(0,1),RationalNumber(0,1)],
@@ -54,43 +51,9 @@ samples = [[RationalNumber(0,1)],
            [RationalNumber(0,1),RationalNumber(0,1),RationalNumber(0,1),RationalNumber(0,1),RationalNumber(0,1)],
            [RationalNumber(0,1),RationalNumber(0,1),RationalNumber(0,1),RationalNumber(0,1),RationalNumber(0,1),RationalNumber(0,1)]]
 
-class RNNInterface:
 
-    def __init__(self):
-        self.w2v_model = load_model(word2vecModelPath, limit=3000)
-        RNN_model = RNNModel(input_size=300, hidden_size=64,
-                             output_size=1, num_layers=2, model_name=RNNModelName)
-        # Load the model saved already in the models folder
-        RNN_model.load_RNN_model(RNNModelPath)
-        print ("RNN loaded successfully! {RNN_model}")
-        self.rnn_model = RNN_model
-
-    def convertRationalNumberListToWordList(self, string: list):
-        #TODO: Write a new function to convert directly Rational Num to Tensor, rather than converting to word first. in RNN
-        is_denominator_same = all(x.getDenominator() == string[0].getDenominator() for x in string)
-        if is_denominator_same or string == []:
-            # if len(string) > 1 and \
-            #    all((x == string[1] and string[1] != string[0]) for x,i in zip(string,range(len(string))) if i%2 == 1):
-            #     wordL = [self.w2v_model.index_to_key[x.getNumerator()] if i%2==0 else '#' for x,i in zip(string,range(len(string)))]
-            # else:
-            wordL = [self.w2v_model.index_to_key[x.getNumerator()] for x in string]
-            return wordL
-        else:
-            raise("ERROR: NOT IMPLEMENTED YET!") #TODO: implement this later
-
-    def askRNN(self, NumList: list):
-        wordL = self.convertRationalNumberListToWordList(NumList)
-        print(f"The converted query to RNN is:{wordL}")
-        X = [encode_sequence(wordL, self.w2v_model)]
-        RNN_reply =  self.rnn_model.test_RNN(X, None)
-        if len(RNN_reply) != 1:
-            raise Exception(f"Since, the student asks teacher one query at a time, "
-                            "the length of answer from Teacher i.e. RNN should be exactly One")
-        return RNN_reply[0]
-
-
-rnnInterface = RNNInterface()
-gTComparison = GTComparison(Lang_is_abSeq)
+rnnInterface = RNNInterface(word2vec_model_path=word2vecModelPath, rnn_model_path=RNNModelPath)
+gTComparison = GTComparison(Lang_is_aStar)
 def membershipQuery(word : list, printing = True) -> bool:
     #expects a list of RationalNumbers
     if len(word) and type(word[0]) != type(RationalNumber(None,None)):
@@ -101,7 +64,7 @@ def membershipQuery(word : list, printing = True) -> bool:
     # print (Qreply)
     if Qreply:
         if printing:
-            print(f"membershipQuery: {word} matches the pattern abab.")
+            print(f"membershipQuery: {word} is in the language.")
     else:
         if printing:
             print(f"membershipQuery: {word} is not in the language.")
