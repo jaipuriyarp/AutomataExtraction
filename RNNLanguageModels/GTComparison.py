364869eb0df061f48094613cf9b721f1f303293e
pravriti <jaipuriy@rhrk.uni-kl.de>
2023-06-26 17:56:43 +0200

    Added some more print statements

--- RNNLanguageModels/GTComparison.py
+++ RNNLanguageModels/GTComparison.py
@@ -2,6 +2,7 @@ class GTComparison:
 
     def __init__(self, memQ_for_GT):
         self.queries = {}
+        self.qCount  = 0
         #actual count:
         self.num_pos_memQ = 0
         self.num_neg_memQ = 0
@@ -33,6 +34,7 @@ class GTComparison:
     def getGT(self, word:list , RNN_ans, memQ): # word: list[RationalNUmbers]
         '''This function is used to compare the GT with RNN answer on the fly with the function provided to it'''
         currKey = tuple(word)
+        self.qCount += 1
         if currKey in self.queries.keys():
             actual_ans =  self.queries[currKey]
         else:
@@ -87,8 +89,14 @@ class GTComparison:
     def statistics(self):
         '''This function shows statistics'''
 
-        print('*' * 40)
-        print(f"Total Number of queries asked                                      : {len(self.queries)}")
+        print('*' * 100)
+        print(f"Total Number of queries asked                                      : {self.qCount}")
+        print(f"Total number of pos queries asked                                  : {self.num_pos_memQ + self.num_pos_EquivQ}")
+        print(f"Total number of neg queries asked                                  : {self.num_neg_memQ + self.num_neg_EquivQ}")
+        print(f"Total number of queries answered correctly by RNN                  : {self.true_positive + self.true_negative}")
+        print(f"Accuracy                                                           : {(self.true_positive + self.true_negative) /self.qCount}")
+        print('*' * 100)
+
         print(f"Total Number of membership queries asked                           : {self.num_pos_memQ + self.num_neg_memQ}")
         print(f"Total Number of positive membership queries asked                  : {self.num_pos_memQ}")
         print(f"Total Number of negative membership queries asked                  : {self.num_neg_memQ}")
@@ -97,33 +105,29 @@ class GTComparison:
         print(f"Total Number of positive queries asked during Equivalence check    : {self.num_pos_EquivQ}")
         print(f"Total Number of negative queries asked during Equivalence check    : {self.num_neg_EquivQ}")
 
-        print('*' * 40)
-        print(f"Total Number of queries answered by RNN correctly                  : {self.true_positive + self.true_negative}")
-        print(f"Accuracy %                                                         : "
-              f"{(self.true_positive + self.true_negative)/ (self.num_pos_memQ + self.num_neg_memQ + self.num_pos_EquivQ + self.num_neg_EquivQ)}")
 
         ## membership queries:
-        print('*' * 40)
+        print('*' * 100)
         print(f"Total Number of membership queries answered by RNN correctly          : "
               f"{(self.true_positive_memQ + self.true_negative_memQ)}")
-        print(f"Accuracy %                                                            : "
+        print(f"Accuracy of membership queries answered                               : "
               f"{(self.true_positive_memQ + self.true_negative_memQ) / (self.num_pos_memQ + self.num_neg_memQ)}")
         print(f"Total Number of positive membership queries answered by RNN correctly : {self.true_positive_memQ}")
-        print(f"Accuracy                                                              : {self.true_positive_memQ/self.num_pos_memQ}")
+        print(f"Accuracy of +ve membership queries                                    : {self.true_positive_memQ/self.num_pos_memQ}")
         print(f"Total Number of negative membership queries answered by RNN correctly : {self.true_negative_memQ}")
-        print(f"Accuracy                                                              : {self.true_negative_memQ / self.num_neg_memQ}")
+        print(f"Accuracy of -ve membership queries                                    : {self.true_negative_memQ / self.num_neg_memQ}")
 
         ## Equivalence queries:
-        print('*' * 40)
+        print('*' * 100)
         print(f"Total Number of queries answered during equivalence check by RNN correctly          : "
             f"{(self.true_positive_EquivQ + self.true_negative_EquivQ)}")
-        print(f"Accuracy %                                                                          : "
+        print(f"Accuracy of equivalence queries answered                                            : "
               f"{(self.true_positive_EquivQ + self.true_negative_EquivQ) / (self.num_pos_EquivQ + self.num_neg_EquivQ)}")
         print(f"Total Number of positive queries answered during equivalence check by RNN correctly : {self.true_positive_EquivQ}")
-        print(f"Accuracy                                                                            : "
+        print(f"Accuracy of +ve queries asked during equivalence check                              : "
               f"{self.true_positive_EquivQ/ self.num_pos_EquivQ}")
         print(f"Total Number of negative queries answered during equivalence check by RNN correctly : {self.true_negative_EquivQ}")
-        print(f"Accuracy                                                                            : "
+        print(f"Accuracy of -ve queries asked during equivalence check                              : "
               f"{self.true_negative_EquivQ / self.num_neg_EquivQ}")
 
 
