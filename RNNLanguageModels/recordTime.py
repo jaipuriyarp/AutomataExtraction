d84232f70a7e9d477fcd59e49867b780341e0ea7
pravriti <jaipuriy@rhrk.uni-kl.de>
2023-07-16 14:57:50 +0200

    Added stop time recording functionality

--- RNNLanguageModels/recordTime.py
+++ RNNLanguageModels/recordTime.py
@@ -2,6 +2,7 @@ import time
 
 class RecordTime():
     def __init__(self, record_elapsed_time=False):
+        self.real_start = None
         self.start_time = None
         self.end_time = None
         self.end_time_list = []
@@ -10,6 +11,8 @@ class RecordTime():
         if not(self.record_elapsed_time):
             print(f"WARNING: record_elapsed_time parameter is not enabled!!")
     def start(self):
+        if self.real_start is None:
+            self.real_start = time.time()
         self.start_time = time.time()
     def stop(self):
         self.end_time = time.time()
@@ -25,6 +28,7 @@ class RecordTime():
 
     def report(self):
         if self.record_elapsed_time:
+            self.end_time_list = [i - self.real_start for i in self.end_time_list]
             print(f"Elapsed time list: {self.elapsed_time_list}")
             print(f"End time list: {self.end_time_list}")
         else:
